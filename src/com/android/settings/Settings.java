cbbad4d54b8841a8eb1b6a949a75abc6b385989c
Joe Maples <joe@frap129.org>
2017-09-08 14:05:31 -0500

    Settings: Integrate Reptools into Settings
    Based on both DU's Nougat and Omni's Oreo implimentations, so
    credits to mazda and maxwen for this.
    
    Change-Id: Id4e5367bab5826bfc8bced5684fc2e599f8140c4
    Signed-off-by: Joe Maples <joe@frap129.org>
    [jaysonedson: Ported to android11 and Android.bp]
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>
    Signed-off-by: alanndz <alanndz@qmail.id>
    Signed-off-by: alanndz <alanndz7@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -324,4 +324,9 @@ public class Settings extends SettingsActivity {
      * Activity for AppDashboard.
      */
     public static class AppDashboardActivity extends SettingsActivity {}
+
+    /**
+     * Activity for Komodo Settings
+     */
+    public static class ReptoolsActivity extends SettingsActivity {}
 }
