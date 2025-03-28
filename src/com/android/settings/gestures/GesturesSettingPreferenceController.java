dadd4def24955778698eeef1bbfd5ad2d1498cfd
Kry9toN <kry9ton@kryptonproject.my.id>
2025-03-28 09:44:37 +0000

    Settings: Add three-fingers-swipe to screenshot [3/3]
    Co-authored-by: wulan17 <wulan17@nusantararom.org>
    Co-authored-by: ghbhaha <ghbhaha@gmail.com>

--- src/com/android/settings/gestures/GesturesSettingPreferenceController.java
+++ src/com/android/settings/gestures/GesturesSettingPreferenceController.java
@@ -27,6 +27,8 @@ import com.android.settingslib.core.AbstractPreferenceController;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.komodo.settings.category.gestures.SwipeToScreenshotPreferenceController;
+
 public class GesturesSettingPreferenceController extends BasePreferenceController {
     private List<AbstractPreferenceController> mGestureControllers;
 
@@ -70,6 +72,7 @@ public class GesturesSettingPreferenceController extends BasePreferenceControlle
         controllers.add(new PreventRingingParentPreferenceController(context, FAKE_PREF_KEY));
         controllers.add(new ScreenOffUdfpsPreferenceController(context, FAKE_PREF_KEY)
                 .setConfig(ambientDisplayConfiguration));
+        controllers.add(new SwipeToScreenshotPreferenceController(context, FAKE_PREF_KEY));
         return controllers;
     }
 }
